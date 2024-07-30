cb0c97361180febf2629a6261a3b5fd80d58121f
mhuseynn <hmehdiyev2005@gmail.com>
2024-07-30 18:35:59 +0400

    final1

---
+++ ProductTask/Controllers/ProductController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductTask.Models;
+
+namespace ProductTask.Controllers;
+
+public class ProductController : Controller
+{
+
+    public static List<Product> Products { get; set; }
+
+
+    static ProductController()
+    {
+        Products = new List<Product>();
+
+        for (int i = 1; i <= 20; i++)
+        {
+            Products.Add(new Product
+            {
+                Id = i,
+                Name = "Product " + i,
+                Price = 10
+            });
+        }
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+
+    public IActionResult GetAllProducts()
+    {
+        return View(Products);
+    }
+
+
+    public IActionResult GetProductById(int id)
+    {
+
+        var product = Products?.FirstOrDefault(p => p.Id == id);
+        return View(product);
+
+    }
+
+    public IActionResult DeleteProductById(int id)
+    {
+        var product = Products?.FirstOrDefault(p => p.Id == id);
+        if (product != null)
+        {
+            Products?.Remove(product);
+        }
+        return RedirectToAction("GetAllProducts");
+    }
+
+
+}
---
+++ ProductTask/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿namespace ProductTask.Models;
+
+public class Product
+{
+    
+
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public int Quantity { get; set; }
+
+    public double Price { get; set; }
+
+
+
+    public override string ToString()
+    {
+        return $@"{Id}----{Name}---{Quantity}---{Price}";
+    }
+
+}
